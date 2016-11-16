1d7434141cc362a0017ab8adf58925fd87ebb18d
davych@163.com <davych@163.com>
2016-11-16 11:36:28 +0800

    ---

--- src/modules/user/userMgr.vue
+++ src/modules/user/userMgr.vue
@@ -19,7 +19,7 @@
                     <formdim labelname="用户名："  placeholder="姓名"  dimlabel="CardName" querylabel="CardName" :value="addFormData.CardCode" :iptvalue="addFormData.CardName" id="CardCode"  @fromdim="formDimClick"  formname='CardName' :validatestart="newForm.validate" @onvalidate="newForm.validateHandler" url="employees" :params="dimParams"></formdim>
                     <formtext labelname="系统账号："  :value.sync="addFormData.CardCode" :must="false"  :vertical="true" :read="true" :ingnore='true' ></formtext>
                     <formtext labelname="用户电话：" :value.sync="addFormData.phone"  :must="false" :vertical="true" :read="true" :ingnore='true' ></formtext>
-                    <formrd labelname="是否启用：" :vertical="true" formname="U_Type" :value.sync="addFormData.U_Type" :datas="[{label:'是', id:'N', checked: false},{label:'否', id:'F', checked: false},]"  :validatestart="newForm.validate" @onvalidate="newForm.validateHandler"></formrd>
+                    <formrd labelname="是否启用：" :vertical="true" formname="ValidFor" :value.sync="addFormData.ValidFor" :datas="[{label:'是', id:'Y', checked: false},{label:'否', id:'N', checked: false},]"  :validatestart="newForm.validate" @onvalidate="newForm.validateHandler"></formrd>
                     <formck labelname="角色：" :vertical="true" formname="roles" lname="name" lkey="name" :value.sync="addFormData.roles"  :datas="getRoles" :validatestart="newForm.validate" @onvalidate="newForm.validateHandler"></formck>
               </div>
         </dialog>
@@ -45,7 +45,7 @@ import pageBase from "common/mixinPage.js";
 import dialogtip from "component/dialog/dialogTip";
 import Utils from "common/Utils";
 let headerData = [{name:"用户名", labelValue:"CardCode", type:"data"},{name:"姓名", labelValue:"CardName", type:"data"},{name:"角色", labelValue:"roles",type:"data"},
-                  {name:"状态", labelValue:"U_Type_desc",type:"data",adapterFun: function(d) {return d.U_Type =="N"?"启用":"禁用"}},{name:"创建人", labelValue:"createdByName",type:"data"},
+                  {name:"状态", labelValue:"ValidFor_desc",type:"data",adapterFun: function(d) {return d.ValidFor =="Y"?"启用":"禁用"}},{name:"创建人", labelValue:"createdByName",type:"data"},
                   {name:"创建时间", labelValue:"createAt_desc", type:"data",adapterFun: function(d) {return Utils.formate(new Date(d.createAt), "yyyy-mm-dd");}},{type:"operator", name:"操作"}]
 export default {
   mixins:[pageBase],
@@ -83,21 +83,21 @@ export default {
           CardCode:"",
           CardName:"",
           phone:"",
-          U_Type:"N",
+          ValidFor:"Y",
           roles:""
       },
       tableEvents:{
         operatorRender: function(d){
           let bdatas = [{name:"密码重置", action:"rebuild",icon:"icon-key", data:d},{name:"编辑", action:"edit",icon:"icon-edit", data:d}];
-          if(d.U_Type == "F") bdatas.unshift({name:"启用", action:"open", icon:"icon-check", data:d})
+          if(d.ValidFor == "N") bdatas.unshift({name:"启用", action:"open", icon:"icon-check", data:d})
           else bdatas.unshift({name:"禁用", action:"forbidden", icon:"icon-forbidden", data:d});
           return bdatas;
         },
 
         operatorHandler: function(d){
           this.curopData = Utils.cloneObj(d.data);
-          if(d.action == "open") this.changeOpen("N");
-          else if(d.action == "forbidden") this.changeOpen("F");
+          if(d.action == "open") this.changeOpen("Y");
+          else if(d.action == "forbidden") this.changeOpen("N");
           else if(d.action == "rebuild") this.$set("resetDialog", !this.resetDialog);
           else if(d.action == "edit") this.$set("showRolesDialog", !this.showRolesDialog);
         }
@@ -114,7 +114,7 @@ export default {
     sdata: function(){
       let q = this.$route.query;
       return [{type:"text",  value:q.CardName || "",  keyname:"CardCode", labelcaption:"用户名:"},
-              {type:"combobox", keyname:"U_Type", labelname:"name", keyid:"id", value:q.U_Type || "", datas:[{name:"启用", id: 'N'}, {name:"禁用", id:"F"}], labelcaption:"用户状态"},
+              {type:"combobox", keyname:"ValidFor", labelname:"name", keyid:"id", value:q.ValidFor || "", datas:[{name:"启用", id: 'Y'}, {name:"禁用", id:"N"}], labelcaption:"用户状态"},
               {type:"daterange",  keynamestart:"start", keynameend:"end", start:q.start || "",  end:q.end || "", formate:"yyyy-mm-dd", labelcaption:"创建时间:"}];
     },
     getRoles: function(){
@@ -141,7 +141,7 @@ export default {
                       this.showFormDialog = !this.showFormDialog;
                       this.loadlist();
                       this.showMsg("success", "新增成功");
-                      this.addFormData = {CardCode:"", CardName:"",phone:"",U_Type:"N",roles:""}
+                      this.addFormData = {CardCode:"", CardName:"",phone:"",ValidFor:"Y",roles:""}
                   },(error) =>{
                     this.showMsg("error", error.msg);
                   })
@@ -164,7 +164,7 @@ export default {
         }
     },
     changeOpen: function(type){
-        this.changeOne({CardCode: this.curopData.CardCode, U_Type:type})
+        this.changeOne({CardCode: this.curopData.CardCode, ValidFor:type})
     },
     changeOne: function(params, cb){
       this.$http.put(this.$Api+"users",JSON.stringify(params)).then((res) => {
@@ -172,7 +172,7 @@ export default {
           if(cb) cb()
           this.showMsg("success", "修改成功");
           this.loadlist();
-          // this.addFormData = {CardCode:"", CardName:"",phone:"",U_Type:"N",roles:""}
+          // this.addFormData = {CardCode:"", CardName:"",phone:"",ValidFor:"N",roles:""}
       },(error) =>{
         this.showMsg("error", error.msg);
       })
