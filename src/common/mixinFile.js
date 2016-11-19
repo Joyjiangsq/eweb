c8e71b7c8b219583aa74596ce214c3cac5518283
davych@163.com <davych@163.com>
2016-11-19 16:26:59 +0800

    '--'

--- src/common/mixinFile.js
+++ src/common/mixinFile.js
@@ -49,7 +49,7 @@ let fileMixins = {
                       self.$dispatch("upsuccess", {url: data.url, name:self.file.name});
                     }
                     else {
-                      if(self.tipvalidate) self.tipvalidate(data);
+                      if(self.tipvalidate) self.tipvalidate.call(self.context,data);
                       else showTips(self.$store, {type:"warn", msg:data.msg});
                     }
                 },
